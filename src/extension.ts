9f5d8756433e5993a00a33f973edaa6199d4d7c4
Frida Rosenaa <macrosedk@gmail.com>
2025-09-23 16:41:48 +0200

    Add brand, material and filament

--- src/extension.ts
+++ src/extension.ts
@@ -23,11 +23,23 @@ let singlePurchaseLink = `,
 				"affiliate": false
 			}`;
 
+async function addSize(editor: vscode.TextEditor, edit: any) {
+	editor.selections.forEach((selection, i) => {
+        edit.replace(selection, singleSizeData)
+	});
+}
+
+async function addPurchase(editor: vscode.TextEditor, edit: any) {
+	editor.selections.forEach((selection, i) => {
+        edit.replace(selection, singlePurchaseLink)
+	});
+}
+
 function replaceAtStr(target: string, index: number, replacement: string) {
     return target.substring(0, index) + replacement + target.substring(index + replacement.length);
 }
 
-function preprocessVariantName(name: string) {
+function preprocessName(name: string) {
 	const strArr = name.split(/(\s+)/);
 	let tmpArr: string[] = []
 
@@ -55,7 +67,7 @@ async function newVariant(location: any) {
 		return;
 	}
 	
-	variantName = preprocessVariantName(variantName);
+	variantName = preprocessName(variantName);
 
 	let variantColour = await vscode.window.showInputBox({
 		placeHolder: 'And what is its colour hex?',
@@ -118,22 +130,150 @@ async function newVariant(location: any) {
 	vscode.window.showTextDocument(sizesFile);
 }
 
-async function addSize(editor: vscode.TextEditor, edit: any) {
-	editor.selections.forEach((selection, i) => {
-        edit.replace(selection, singleSizeData)
+async function newFilament(location: any) {
+	let filamentName = await vscode.window.showInputBox({
+		placeHolder: 'What is your filament called?',
+		ignoreFocusOut: true
 	});
+
+	if (filamentName === undefined) {
+		vscode.window.showErrorMessage("No filament name");
+		return;
+	}
+
+	let density: string|number|undefined = await vscode.window.showInputBox({
+		placeHolder: 'And what is its density?',
+		ignoreFocusOut: true
+	});
+
+	if (density === undefined || !Number.parseFloat(density)) {
+		vscode.window.showErrorMessage("Invalid density");
+		return;
+	}
+
+	density = Number.parseFloat(density.replaceAll(",", "."));
+
+	const newFolder = `${location}/${filamentName}`
+	const folderURI = vscode.Uri.parse(newFolder);
+	const fs = vscode.workspace.fs;
+
+	await fs.createDirectory(folderURI);
+	
+	const filamentFile = folderURI.with({ path: posix.join(folderURI.path, "filament.json") });
+
+	let filamentData = {
+		"name": filamentName,
+		"diameter_tolerance": 0.02,
+		"density": density
+	}
+
+	await fs.writeFile(filamentFile, Buffer.from(JSON.stringify(filamentData, null, 2), "utf8"));
+	vscode.window.showTextDocument(filamentFile);
 }
 
-async function addPurchase(editor: vscode.TextEditor, edit: any) {
-	editor.selections.forEach((selection, i) => {
-        edit.replace(selection, singlePurchaseLink)
+async function newMaterial(location: any) {
+	let materialName = await vscode.window.showInputBox({
+		placeHolder: 'What is your material called?',
+		ignoreFocusOut: true
 	});
+
+	if (materialName === undefined) {
+		vscode.window.showErrorMessage("No material name");
+		return;
+	}
+
+	const newFolder = `${location}/${materialName}`
+	const folderURI = vscode.Uri.parse(newFolder);
+	const fs = vscode.workspace.fs;
+
+	await fs.createDirectory(folderURI);
+	
+	const materialFile = folderURI.with({ path: posix.join(folderURI.path, "brand.json") });
+
+	let materialData = {
+		"material": materialName,
+	}
+
+	await fs.writeFile(materialFile, Buffer.from(JSON.stringify(materialData, null, 2), "utf8"));
+	vscode.window.showTextDocument(materialFile);
+}
+
+async function newBrand(location: any) {
+	let brandName = await vscode.window.showInputBox({
+		placeHolder: 'What is your material called?',
+		ignoreFocusOut: true
+	});
+
+	if (brandName === undefined) {
+		vscode.window.showErrorMessage("No brand name");
+		return;
+	}
+
+	let websiteURL = await vscode.window.showInputBox({
+		placeHolder: 'and whats the website url?',
+		ignoreFocusOut: true
+	});
+
+	if (websiteURL === undefined) {
+		vscode.window.showErrorMessage("No brand website");
+		return;
+	}
+
+	let origin = await vscode.window.showInputBox({
+		placeHolder: 'and whats its origin country code? (OPTIONAL)'
+	});
+
+	if (origin === undefined) {
+		origin = "unknown";
+	}
+
+	const newFolder = `${location}/${brandName}`
+	const folderURI = vscode.Uri.parse(newFolder);
+	const fs = vscode.workspace.fs;
+
+	await fs.createDirectory(folderURI);
+	
+	const brandFile = folderURI.with({ path: posix.join(folderURI.path, "material.json") });
+
+	let brandData = {
+		"brand": brandName,
+		"website": websiteURL,
+		"logo": "FILENAME.png, place it next to this file, it needs to be 400x400, svg, png or jpg",
+		"origin": origin
+	}
+
+	await fs.writeFile(brandFile, Buffer.from(JSON.stringify(brandData, null, 2), "utf8"));
+	vscode.window.showTextDocument(brandFile);
 }
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('extension.newVariant', async (params) => {
 		try {
 			await newVariant(params);
+		} catch (e) {
+			vscode.window.showErrorMessage(`Error ${e}`)
+		}
+	});
+
+	vscode.commands.registerCommand('extension.newFilament', async (params) => {
+		try {
+			await newFilament(params);
+		} catch (e) {
+			vscode.window.showErrorMessage(`Error ${e}`)
+		}
+	});
+
+	vscode.commands.registerCommand('extension.newMaterial', async (params) => {
+		try {
+			await newMaterial(params);
+		} catch (e) {
+			vscode.window.showErrorMessage(`Error ${e}`)
+		}
+	});
+
+	vscode.commands.registerCommand('extension.newBrand', async (params) => {
+		try {
+			await newBrand(params);
 		} catch (e) {
 			vscode.window.showErrorMessage(`Error ${e}`)
 		}
